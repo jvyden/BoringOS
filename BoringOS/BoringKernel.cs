6214ce0e434e29a44ec945d32333dbd834e9464d
jvyden <jvyden@jvyden.xyz>
2023-06-19 19:34:50 -0400

    Initial Commit

---
+++ BoringOS/BoringKernel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CosmosKernel = Cosmos.System.Kernel;
+
+namespace BoringOS;
+
+public class Kernel : CosmosKernel
+{
+    protected override void BeforeRun()
+    {
+        Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+    }
+
+    protected override void Run()
+    {
+        Console.Write("Input: ");
+        var input = Console.ReadLine();
+        Console.Write("Text typed: ");
+        Console.WriteLine(input);
+    }
+}
