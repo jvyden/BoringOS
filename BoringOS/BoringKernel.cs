2959bb3651d1f3442eb4204feef837a9d2b86673
jvyden <jvyden@jvyden.xyz>
2023-06-19 20:03:20 -0400

    It now opens

--- BoringOS/BoringKernel.cs
+++ BoringOS/BoringKernel.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using CosmosKernel = Cosmos.System.Kernel;
+﻿using Cosmos.System;
+using JetBrains.Annotations;
+using Console = System.Console;
 
 namespace BoringOS;
 
-public class Kernel : CosmosKernel
+public class BoringKernel : Kernel
+
 {
+    [UsedImplicitly]
     protected override void BeforeRun()
     {
         Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
     }
 
+    [UsedImplicitly]
     protected override void Run()
     {
         Console.Write("Input: ");
-        var input = Console.ReadLine();
+        string input = Console.ReadLine();
         Console.Write("Text typed: ");
         Console.WriteLine(input);
     }
---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Sourced from https://cosmosos.github.io/articles/Installation/Running.html
+
+helpFunction()
+{
+   printf "\n"
+   printf "Usage: %s -i <ISO> -m <memory size>\n" "$0"
+   printf "\t-i ISO path to be used\n"
+   printf "\t-m Memory size to allocate to the Virtual Machine\n"
+#   printf "\t-h Hard disk image location, can be created with qemu-img\n"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:m:" opt
+do
+   case "$opt" in
+      i ) ISO="$OPTARG" ;;
+      m ) MEMORY_SIZE="$OPTARG" ;;
+#      h ) HDD_IMAGE="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$ISO" ] || [ -z "$MEMORY_SIZE" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Emulate the ISO
+qemu-system-x86_64 -boot d -cdrom "$ISO" -m "$MEMORY_SIZE"
