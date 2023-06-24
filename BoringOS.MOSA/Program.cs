ddc34486b5931077ff6d9d69c439b85d84f4743d
jvyden <jvyden@jvyden.xyz>
2023-06-24 16:38:56 -0400

    MOSA base

---
+++ BoringOS.MOSA/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using System;
+
+namespace BoringOS.MOSA
+{
+    public static class Program
+    {
+        public static void Setup()
+        {
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Loop()
+        {
+        }
+    }
+}
